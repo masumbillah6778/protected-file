5f507dd4da304627c57dd177a644b883997f3aab
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-12-03 01:03:54 +0600

    Update protected-file.js

--- secret/js/protected-file.js
+++ secret/js/protected-file.js
@@ -1,35 +1,14 @@
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-  }
+var password = document.getElementById('password');
+      var toggler = document.getElementById('toggler');
 
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
+      showHidePassword = () => {
+        if (password.type == 'password') {
+          password.setAttribute('type', 'text');
+          toggler.classList.add('fa-eye-slash');
+        } else {
+          toggler.classList.remove('fa-eye-slash');
+          password.setAttribute('type', 'password');
+        }
+      };
 
-
-
-  function checkCookie() {
-    var isProtected = getCookie("passwordProtected");
-    if (isProtected) {
-      document.getElementById("protected-content").style.display = "block";
-    } else {
-      document.getElementById("password-form").style.display = "block";
-    }
-  }
-
-  window.onload = function() {
-    checkCookie();
-  }
+      toggler.addEventListener('click', showHidePassword);
