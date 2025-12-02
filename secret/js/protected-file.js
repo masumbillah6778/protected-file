028f075ab1aae241b34faffea64fa0e8f2013bc9
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-12-02 23:26:07 +0600

    Create protected-file.js

---
+++ secret/js/protected-file.js
@@ -0,0 +1,40 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+
+      document.getElementById("password-form").style.display = "none";
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  }
+
+  function checkCookie() {
+    var isProtected = getCookie("passwordProtected");
+    if (isProtected) {
+      document.getElementById("protected-content").style.display = "block";
+    } else {
+      document.getElementById("password-form").style.display = "block";
+    }
+  }
+
+  window.onload = function() {
+    checkCookie();
+  }
